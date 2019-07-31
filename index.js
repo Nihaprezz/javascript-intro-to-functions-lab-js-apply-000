d0040fe82f910843e425389991b80a0219450299
Sergio Perez <sergio13prez@gmail.com>
2019-07-31 23:41:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,13 +14,9 @@ function logWhisper(string){
   console.log(string.toLowerCase());
 }
 
-function sayHiToGrandma(string){ 
-  if (string.toLowerCase === string){
-    return "I can\'t hear you!";
-  } else if (string === string.toUpperCase){
-    return "YES INDEED!";
-  } else if (string === "I love you, Grandma."){
-    return "I love you, too.";
+function sayHiToGrandma(string){
+  if(string === string.toLowerCase){
+    return 'is lowercase';
   }
 }
 
